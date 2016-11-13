ba33dfd707c615b54f9dbaf12e29322914761891
Takahito Kito <kito.takahito@ist.osaka-u.ac.jp>
2016-11-14 01:50:31 +0900

    ネットワーク情報をJSON形式で出力するクラス

---
+++ lib/view/visJs.rb
@@ -0,0 +1,42 @@
+require 'json'
+
+module View
+  # Topology controller's GUI (vis).
+  class VisJs
+    def initialize(output = 'topology.json')
+      @output = output
+      @nodes = Array.new { [] }
+    end
+
+    # rubocop:disable AbcSize
+    def update(_event, _changed, topology)
+      nodes = topology.switches.each_with_object({}) do |each, tmp|
+        tmp[each] = { "id"=> each, "label"=> each.to_hex }
+      end
+      i = 0
+      links = topology.links.each_with_object({}) do |each, tmp|
+        next unless nodes[each.dpid_a] && nodes[each.dpid_b]
+        tmp[i] = { "from"=> each.dpid_a, "to"=> each.dpid_b }
+        i += 1
+      end
+      i = 0
+      hosts = topology.hosts.each_with_object({}) do |each, tmp|
+        tmp[i] = { "id"=> nodes.length+i+1, "label"=> each[1].to_s }
+        i += 1
+      end
+      i = 0
+      h_links = topology.hosts.each_with_object({}) do |each, tmp|
+        tmp[nodes.length+i] = { "from"=> each[2], "to"=> nodes.length+i+1 }
+        i += 1
+      end
+      open(@output, "w") do |io|
+        JSON.dump([ "nodes"=> nodes.values, "hosts"=> hosts.values, "links"=> links.values], io)
+      end
+    end
+    # rubocop:enable AbcSize
+
+    def to_s
+      "vizJs mode, output = #{@output}"
+    end
+  end
+end
